4d69d52d69f7f4f4cf8b94f5f28c9b05c06fafae
Gogs <gogs@fake.local>
2024-05-06 15:44:51 +0200

    login

--- app/src/main/java/com/example/kursproject/ui/LoginRegister/LoginFragment.kt
+++ app/src/main/java/com/example/kursproject/ui/LoginRegister/LoginFragment.kt
@@ -1,13 +1,16 @@
 package com.example.kursproject.ui.LoginRegister
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.findNavController
 import com.example.kursproject.R
 import com.example.kursproject.databinding.FragmentLoginBinding
+import com.example.kursproject.ui.MainActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -30,8 +33,9 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
                     task->
                 if (task.isSuccessful) {
                     val user = auth.currentUser
-//                    var intent=Intent(requireContext(),MainActivity::class.java)
-//                    startActivity(intent)
+                    Toast.makeText(requireContext(),"User"+user,Toast.LENGTH_LONG).show()
+                    var intent=Intent(requireActivity(),MainActivity::class.java)
+                    startActivity(intent)
                 } else {
 
                 }
--- app/src/main/java/com/example/kursproject/ui/MainActivity.kt
+++ app/src/main/java/com/example/kursproject/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.kursproject.R
